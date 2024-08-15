1aa6023d60a9f8bced2f950ca54fbf14ca97f8ec
Pablo Vargas <49590991+PabloVarg@users.noreply.github.com>
2024-08-14 19:36:52 -0600

    feat(scheduler): Handle task state updates

--- scheduler/grpc.go
+++ scheduler/grpc.go
@@ -2,6 +2,7 @@ package scheduler
 
 import (
 	"context"
+	"time"
 
 	pb "github.com/pablovarg/distributed-task-scheduler/internal/grpc"
 )
@@ -20,4 +21,26 @@ func (s *SchedulerServerImpl) SendHeartbeat(ctx context.Context, heartbeat *pb.H
 
 	return &pb.Ok{}, nil
 }
+
+func (s *SchedulerServerImpl) UpdateJobStatus(ctx context.Context, task *pb.TaskStatus) (*pb.Ok, error) {
+	s.logger.Printf("update task %d with status %s", task.GetID(), task.GetState())
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	var err error
+	switch task.GetState() {
+	case pb.TaskState_PICKED:
+		err = s.taskModel.PickTask(ctx, int(task.GetID()))
+	case pb.TaskState_SUCCESS:
+		err = s.taskModel.CompleteTask(ctx, int(task.GetID()))
+	case pb.TaskState_FAILED:
+		err = s.taskModel.FailTask(ctx, int(task.GetID()))
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.Ok{}, nil
 }
