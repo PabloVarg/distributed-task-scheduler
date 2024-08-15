3cdc788d6375707f962c883c2b14d5422f82830b
Pablo Vargas <49590991+PabloVarg@users.noreply.github.com>
2024-08-14 19:33:33 -0600

    feat(migrations): Complete state timestamps

--- migrations/000001_create_task_table.up.sql
+++ migrations/000001_create_task_table.up.sql
@@ -1,7 +1,13 @@
 CREATE TABLE task (
     id bigserial PRIMARY KEY,
     command text,
-    created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
     scheduled_at timestamp(0) with time zone,
-    successful_at timestamp(0) with time zone
+
+    -- Status timestamps
+    picked_at timestamp(0) with time zone,
+    successful_at timestamp(0) with time zone,
+    failed_at timestamp(0) with time zone,
+
+    -- Timestamps
+    created_at timestamp(0) with time zone NOT NULL DEFAULT NOW()
 );
